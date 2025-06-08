f5f514686cc80b237d40cbebc75b03d540098290
AkshaySujesh <71964076+AkshayS2020git@users.noreply.github.com>
2025-06-08 14:14:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 import mysql.connector
 
+#!Author - Akshay Sujesh 
+
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
